5b08bb776c843ae8ce0b82842ec9392764387b95
quanhm <quanhoang765e@gmail.com>
2024-07-31 09:13:41 +0700

    #setup - add GlobalExceptionHandle

--- src/main/java/com/sevenhallo/common/exception/GlobalExceptionHandle.java
+++ src/main/java/com/sevenhallo/common/exception/GlobalExceptionHandle.java
@@ -1,4 +1,15 @@
 package com.sevenhallo.common.exception;
 
+import com.sevenhallo.common.exception.custom.AbsApplicationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandle {
+
+    @ExceptionHandler(AbsApplicationException.class)
+    public ResponseEntity<?> applicationExceptionHandle(AbsApplicationException e) {
+        return e.makeResponseData();
+    }
 }
